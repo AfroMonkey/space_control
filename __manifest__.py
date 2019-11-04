021a0b8626ec3012e93584d9937c19d08d2a9a70
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2019-11-04 17:16:21 -0600

    [MIG] 12.0 -> 13.0

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Space Control',
-    'version': '13.0.1.1.0',
+    'version': '13.0.1.1.1',
     'author': 'Navarro Moisés',
     'website': 'https://github.com/AfroMonkey/space_control',
     'category': 'Operations',
