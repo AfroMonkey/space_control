00abdaf21720858bf07cbe267cd951a8e12fc680
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2019-11-20 17:54:20 -0600

    [IMP] external depends

--- __manifest__.py
+++ __manifest__.py
@@ -2,13 +2,19 @@
 
 {
     'name': 'Space Control',
-    'version': '13.0.1.9.0',
+    'version': '13.0.1.9.1',
     'author': 'Navarro Moisés',
     'website': 'https://github.com/AfroMonkey/space_control',
     'category': 'Operations',
     'depends': [
+        'website_enterprise',
         'point_of_sale',
     ],
+    'external_dependencies': {
+        'python': [
+            'pyOpenSSL',
+        ],
+    },
     'data': [
         'security/space_control_groups.xml',
         'security/ir.model.access.csv',
