53e1d4b3c329bfe8f6b7a7078b9a2a780a16e819
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2019-11-21 18:47:46 -0600

    [REM] website_enterprise

--- __manifest__.py
+++ __manifest__.py
@@ -2,12 +2,11 @@
 
 {
     'name': 'Space Control',
-    'version': '13.0.1.10.0',
+    'version': '13.0.1.10.1',
     'author': 'Navarro Moisés',
     'website': 'https://github.com/AfroMonkey/space_control',
     'category': 'Operations',
     'depends': [
-        'website_enterprise',
         'point_of_sale',
     ],
     'external_dependencies': {
