6b07c5bd57f2604b015af80f2ccf9e6689e7da93
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2019-11-25 20:31:24 -0600

    [IMP] space form

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Space Control',
-    'version': '13.0.1.13.0',
+    'version': '13.0.1.14.0',
     'author': 'Navarro Moisés',
     'website': 'https://github.com/AfroMonkey/space_control',
     'category': 'Operations',
